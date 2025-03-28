c33c118863eba4a0442b430811c9a70736c7bdf1
Emir Tetik <emirtetik.41@hotmail.com>
2025-03-29 02:14:57 +0300

    new cv

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,6 +10,7 @@ const Contact = () => {
     i18n.language === "en"
       ? "/cv/EmirTetik.en.pdf"
       : "/cv/EmirTetik.tr.pdf";
+      
   return (
     <div className="pl-12 sm:pl-20 md:pl-20 lg:pl-16 xl:pl-28 flex flex-col justify-center min-h-screen text-[var(--text-white)] bg-[var(--text-gray)] overflow-hidden">
       <h1 className="font-prata uppercase  transition-all duration-500 ease-out transform hover:translate-x-4 hover:-skew-x-6 xl:text-[7rem] xl:leading-[7rem] lg:text-[6rem] lg:leading-[6rem] md:text-[5.3rem] md:leading-[5.3rem] sm:text-[4rem] sm:leading-[4rem] max-sm:text-[2.5rem] max-sm:leading-[3rem]">
